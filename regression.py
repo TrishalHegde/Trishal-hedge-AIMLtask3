d972ddffdef9d45bc03297e36e9a40d3e1aad369
Trishal hegde <trishalhegde@gmail.com>
2025-09-28 21:12:17 +0530

    Data preprocessing

--- regression.py
+++ regression.py
@@ -10,3 +10,16 @@ from sklearn.metrics import r2_score, mean_squared_error
 df = pd.read_csv('Housing.csv')
 print("First 5 rows of the dataset:")
 print(df.head())
+print("\nDataset Information:")
+df.info()
+
+binary_cols = ['mainroad', 'guestroom', 'basement', 'hotwaterheating', 'airconditioning', 'prefarea']
+df[binary_cols] = df[binary_cols].apply(lambda x: x.map({'yes': 1, 'no': 0}))
+
+furnishing_dummies = pd.get_dummies(df['furnishingstatus'], drop_first=True, dtype=int)
+df = pd.concat([df, furnishing_dummies], axis=1)
+
+df.drop('furnishingstatus', axis=1, inplace=True)
+
+print("\nDataset after handling categorical variables:")
+print(df.head())
